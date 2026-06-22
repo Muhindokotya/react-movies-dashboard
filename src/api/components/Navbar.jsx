ca6e87664de7307e6fe2f2d6653cfd42ed7ac12c
Muhindo kotya delphin <delvakekya@gmail.com>
2026-06-22 06:46:27 -0700

    Add About page

--- src/api/components/Navbar.jsx
+++ src/api/components/Navbar.jsx
@@ -5,6 +5,8 @@ export default function Navbar() {
                 <span className="navbar-brand">
                     React Movies Dashboard
                 </span>
+                <Link to="/">Home</Link>
+                <Link to="about">About</Link>
             </div>
         </nav>
     );
