a6704bad45537b6b9be4290435a5a65175faa257
Muhindo kotya delphin <delvakekya@gmail.com>
2026-06-22 06:23:50 -0700

    Add About page

--- src/api/pages/AboutPage.jsx
+++ src/api/pages/AboutPage.jsx
@@ -1,12 +1,50 @@
-export default function AboutPage() {
-    return (
-        <div className="container py-5">
-            <h1>About React Movies Dashboard</h1>
+import TechnologyCard from "../components/TechnologyCard";
 
-            <p>
-                This application allows users to search and discover TV shows
-                using data from the TVMaze API.
-            </p>
-        </div>
-    );
+const technologies = [
+  {
+    id: 1,
+    name: "React",
+    description: "JavaScript library for building user interfaces."
+  },
+  {
+    id: 2,
+    name: "React Router",
+    description: "SPA Navigation."
+  },
+  {
+    id: 3,
+    name: "Axios",
+    description: "API Requests."
+  },
+  {
+    id: 4,
+    name: "Bootstrap",
+    description: "Responsive Design."
+  },
+  {
+    id: 5,
+    name: "TVMaze API",
+    description: "Public TV Shows Data."
+  }
+];
+
+export default function AboutPage() {
+  return (
+    <>
+      <h1>About React Movies Dashboard</h1>
+
+      <p>
+        This application allows users to search and discover TV shows
+        using data from the TVMaze API.
+      </p>
+
+      {technologies.map((tech) => (
+        <TechnologyCard
+          key={tech.id}
+          name={tech.name}
+          description={tech.description}
+        />
+      ))}
+    </>
+  );
 }
